460e3672ba24d99ed40c88afcaf6e4f3ff346122
Filipe Deboni <deboni.filipe@gmail.com>
2020-11-12 16:00:44 -0300

    new style applied on Front-End

---
+++ public/precache-manifest.7092c0ab2c7cf4229a86c5c9242dc859.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f69e12aae11a61811842c7d178112bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9fd1738d7930f97c94",
+    "url": "/static/css/main.c0b4ad18.chunk.css"
+  },
+  {
+    "revision": "004c6e2903e6d53f12e2",
+    "url": "/static/js/2.1ba439b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1ba439b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd9fd1738d7930f97c94",
+    "url": "/static/js/main.4e9da82e.chunk.js"
+  },
+  {
+    "revision": "4dde479aaef4cd1efac2",
+    "url": "/static/js/runtime-main.2ef8d8e4.js"
+  },
+  {
+    "revision": "69e3d1db10fd7e17b5dab07bfee8ff28",
+    "url": "/static/media/PetFriendlyLogo.69e3d1db.png"
+  },
+  {
+    "revision": "8c30abe3ed9893838b54c09c798fa6f2",
+    "url": "/static/media/adopt.8c30abe3.png"
+  },
+  {
+    "revision": "0e8c834fef989d6d85f40a5fd55888e4",
+    "url": "/static/media/cute.0e8c834f.png"
+  },
+  {
+    "revision": "9bf5eb1ae808ac75475ce6eb74ce5f25",
+    "url": "/static/media/friends.9bf5eb1a.png"
+  },
+  {
+    "revision": "d2ac81bf70ec09638b891675fbe0df02",
+    "url": "/static/media/home.d2ac81bf.png"
+  }
+]);
